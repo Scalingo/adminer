7660c8ac4c7fa8c32f94d71c86f863209a6a0cd0
François KUBLER <francois+github@kubler.org>
2024-04-02 21:00:57 +0200

    chore: eol

--- index.php
+++ index.php
@@ -1,32 +1,8 @@
 <?php
-function adminer_object() {
-  // required to run any plugin
-  include_once "./plugins/plugin.php";
+$location = getenv("EOL_URL", true);
 
-  // autoloader
-  foreach (glob("plugins/*.php") as $filename) {
-      include_once "./$filename";
-  }
+header("HTTP/1.1 301 Moved Permanently");
+header("Location: " . $location);
 
-  $plugins = array(
-      new AdminerDumpBz2,
-      new AdminerDumpZip,
-      new AdminerEditCalendar,
-      new AdminerEditForeign,
-      new AdminerForeignSystem,
-      new FasterTablesFilter,
-      new AdminerJsonPreview,
-      new AdminerCollations,
-      new AdminerSimpleMenu,
-      // TODO: not ready yet
-      // new AdminerScalingo,
-      new AdminerVersionNoverify,
-      // AdminerTheme MUST always be the latest
-      new AdminerTheme,
-  );
-
-  return new AdminerPlugin($plugins);
-}
-
-// include original Adminer or Adminer Editor
-include "./adminer.php";
+exit;
+?>
